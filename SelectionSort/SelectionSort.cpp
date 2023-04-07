9c59bc4e577006f6caa19a6f0bc6756b1988d399
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-07 09:45:46 +0700

    procedure input

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -36,4 +36,7 @@ void input() {
 	cout << "Masukan Elemen Array" << endl;		
 	cout << "====================" << endl;		
 
+	for (int i = 0; i < n; i++) {					
+		cout << "Data ke" << (i + 1) << ">";		
+		cin >> a[i];
 }
