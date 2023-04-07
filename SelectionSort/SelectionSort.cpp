e1f24337f529a6c40265a1d12630430832d9950c
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-07 09:10:00 +0700

    jawaban no 1

---
+++ SelectionSort/SelectionSort.cpp
@@ -0,0 +1,1 @@
+// 1.Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan serangkaian instruksi langkah demi langkah yang dirancang untuk memecahkan masalah tertentu.
