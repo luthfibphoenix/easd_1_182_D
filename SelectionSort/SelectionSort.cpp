03118f8e26b3b004ae7d9bb4ae52e085e2d17616
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-07 09:53:14 +0700

    membuat procedure selection sort

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -31,12 +31,18 @@ void input() {
 			cout << "\nArray maksimal 20 .\n";
 		}
 	}
-	cout << endl;								
-	cout << "====================" << endl;		
-	cout << "Masukan Elemen Array" << endl;		
-	cout << "====================" << endl;		
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
 
-	for (int i = 0; i < n; i++) {					
-		cout << "Data ke" << (i + 1) << ">";		
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke" << (i + 1) << ">";
 		cin >> a[i];
+	}
 }
+
+void selectionsort(int arr[], int n) {
+
+}
+	
