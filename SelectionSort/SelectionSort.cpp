04bfc933b913bc6af5c2071225b19078bb5d81ec
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-07 09:17:48 +0700

    jawaban no 4

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -1,6 +1,11 @@
 // 1.Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan serangkaian instruksi langkah demi langkah yang dirancang untuk memecahkan masalah tertentu.
+
 // 2.-Linear Data Structure: Linear data structure adalah struktur data yang elemen-elemennya disimpan secara teratur dalam urutan tertentu. Contoh dari struktur data ini adalah array, linked list, stack, dan queue.
 //   -Non-Linear Data Structure: Non-linear data structure adalah struktur data yang elemen-elemennya disimpan tidak dalam urutan tertentu. Contoh dari struktur data ini adalah tree, graph, dan heap. Struktur data ini memungkinkan penyimpanan elemen yang lebih kompleks dan lebih fleksibel dibandingkan dengan linear data structure.
+
 // 3.-Penggunaan Memory: Program algoritma yang efisien harus memanfaatkan memory secara efektif. Jika program menggunakan memory secara berlebihan, hal ini akan memperlambat kinerja program dan mengurangi efisiensi.
 //   -Ukuran Input: Semakin besar ukuran input yang diolah oleh program algoritma, semakin besar pula waktu yang dibutuhkan untuk menyelesaikan algoritma. Oleh karena itu, ukuran input perlu diperhatikan dalam menentukan efisiensi algoritma.
 //   -Algoritma yang Dipilih: Pemilihan algoritma yang tepat juga mempengaruhi efisiensi program.
+
+// 4.SelectionSort karena paling efisien mengurutkan data karena memiliki kompleksitas waktu yang relatif cepat
+
