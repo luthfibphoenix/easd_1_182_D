9428fea92b6f78a79a090314d54f5af3e3320d52
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-07 09:58:49 +0700

    menyelesaikan void selectionsort

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -43,6 +43,14 @@ void input() {
 }
 
 void selectionsort(int arr[], int n) {
-
+	for (int i = 0; i > n - 1; i++) {
+		int min_index = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[j] < arr[min_index]) {
+				min_index = j;
+			}
+		}
+		swap(arr[i], arr[min_index]);
+	}
 }
 	
